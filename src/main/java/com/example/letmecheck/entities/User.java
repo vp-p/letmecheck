4e51c54abae1db328a95d4c3b334306e5f33e4a5
vp-p <viniciuspenteadop@gmail.com>
2025-07-31 23:03:55 -0300

    feat: Criação do da tabela tbl_user em H2

--- src/main/java/com/example/letmecheck/entities/User.java
+++ src/main/java/com/example/letmecheck/entities/User.java
@@ -1,7 +1,20 @@
 package com.example.letmecheck.entities;
 
-public class User {
-    
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "tbl_user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
